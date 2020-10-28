1efb032753c20f65d669a2c6aea78f749b413534
nigelmsipa <nigelmsipa@gmail.com>
2020-10-28 17:33:35 -0400

    i

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const Webpack = require("webpack");
-const Path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
-const FileManagerPlugin = require("filemanager-webpack-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+/** @format */
+
+const Webpack = require('webpack');
+const Path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 const opts = {
   rootDir: process.cwd(),
-  devBuild: process.env.NODE_ENV !== "production"
+  devBuild: process.env.NODE_ENV !== 'production',
 };
 
 module.exports = {
   entry: {
-    app: "./src/js/app.js",
-    settings: "./src/js/settings.js",
-    classic: "./src/scss/classic.scss",
-    corporate: "./src/scss/corporate.scss",
-    modern: "./src/scss/modern.scss"
+    app: './src/js/app.js',
+    settings: './src/js/settings.js',
+    classic: './src/scss/classic.scss',
+    corporate: './src/scss/corporate.scss',
+    modern: './src/scss/modern.scss',
   },
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   devtool:
-    process.env.NODE_ENV === "production" ? "source-map" : "inline-source-map",
+    process.env.NODE_ENV === 'production' ? 'source-map' : 'inline-source-map',
   output: {
-    path: Path.join(opts.rootDir, "dist"),
+    path: Path.join(opts.rootDir, 'dist'),
     pathinfo: opts.devBuild,
-    filename: "js/[name].js"
+    filename: 'js/[name].js',
   },
   performance: { hints: false },
   optimization: {
@@ -35,49 +37,49 @@ module.exports = {
       new TerserPlugin({
         parallel: true,
         terserOptions: {
-          ecma: 5
-        }
+          ecma: 5,
+        },
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin({}),
+    ],
   },
   plugins: [
     // Remove empty js files from /dist
     new FixStyleOnlyEntriesPlugin(),
     // Extract css files to seperate bundle
     new MiniCssExtractPlugin({
-      filename: "css/[name].css",
-      chunkFilename: "css/[id].css"
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[id].css',
     }),
     // jQuery and PopperJS
     new Webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      jquery: "jquery",
-      "window.$": "jquery",
-      "window.jQuery": "jquery",
-      Popper: ["popper.js", "default"]
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery',
+      'window.$': 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
     }),
     // Copy fonts and images to dist
     new CopyWebpackPlugin({
       patterns: [
-        { from: "src/fonts", to: "fonts" },
-        { from: "src/img", to: "img" }
-      ]
+        { from: 'src/fonts', to: 'fonts' },
+        { from: 'src/img', to: 'img' },
+      ],
     }),
     // Speed up webpack build
     new HardSourceWebpackPlugin(),
     // Copy dist folder to docs/dist
     new FileManagerPlugin({
       onEnd: {
-        copy: [{ source: "./dist/**/*", destination: "./docs" }]
-      }
+        copy: [{ source: './dist/**/*', destination: './docs' }],
+      },
     }),
     // Ignore momentjs locales
     new Webpack.IgnorePlugin({
       resourceRegExp: /^\.\/locale$/,
-      contextRegExp: /moment$/
-    })
+      contextRegExp: /moment$/,
+    }),
   ],
   module: {
     rules: [
@@ -85,73 +87,73 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: ["babel-loader?cacheDirectory=true"]
+        loader: ['babel-loader?cacheDirectory=true'],
       },
       // Css-loader & sass-loader
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
-        ]
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       // Load fonts
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "/[name].[ext]",
-              outputPath: "fonts/",
-              publicPath: "../fonts/"
-            }
-          }
-        ]
+              name: '/[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: '../fonts/',
+            },
+          },
+        ],
       },
       // Load images
       {
         test: /\.(png|jpg|jpeg|gif)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "img/",
-              publicPath: "../img/"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: '../img/',
+            },
+          },
+        ],
       },
       // Expose loader
       {
-        test: require.resolve("jquery"),
+        test: require.resolve('jquery'),
         use: [
           {
-            loader: "expose-loader",
-            options: "jQuery"
+            loader: 'expose-loader',
+            options: 'jQuery',
           },
           {
-            loader: "expose-loader",
-            options: "$"
-          }
-        ]
-      }
-    ]
+            loader: 'expose-loader',
+            options: '$',
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".scss"],
-    modules: ["node_modules"],
+    extensions: ['.js', '.scss'],
+    modules: ['node_modules'],
     alias: {
-      request$: "xhr"
-    }
+      request$: 'xhr',
+    },
   },
   devServer: {
-    contentBase: Path.join(__dirname, "docs"),
+    contentBase: Path.join(__dirname, 'docs'),
     compress: true,
     port: 8080,
-    open: true
-  }
+    open: true,
+  },
 };
